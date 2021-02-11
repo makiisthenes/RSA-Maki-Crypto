c2e600b36ad7cc01860dc958e9ef1f4c0c454658
makiisthebes <45903624+makiisthebes@users.noreply.github.com>
2021-02-11 16:10:50 +0000

    "Added Main Method"

---
+++ main.py
@@ -0,0 +1,12 @@
+# RSA Python Implementation
+# Michael Peres
+# 11/02/2021
+
+
+def encrypt(plaintext, n, encrypt_exp) -> bytes:
+    """Function used to encrypt plaintext using public key, x^e mod n gives the ciphertext required."""
+    # Fast exponentiation is required to encrypt.
+
+def decrypt(ciphertext, n, decrypt_exp):
+    """Function used to decrypt ciphertext using private key, y^d mod n gives the plaintext required."""
+    # Fast exponentiation is required to encrypt.
